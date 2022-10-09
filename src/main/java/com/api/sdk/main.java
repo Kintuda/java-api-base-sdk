da80a3e55b258945ebeb1855171cc399e817849c
Kin Tuda <kintuda17@gmail.com>
2022-10-09 16:15:02 -0300

    feat: add client

--- src/main/java/com/api/sdk/main.java
+++ src/main/java/com/api/sdk/main.java
@@ -10,7 +10,7 @@ public class main {
     public static void main(String[] args) throws IOException, SDKException {
         URL url = new URL("https://api.postmon.com.br/v1");
         BaseSDK sdk = new BaseSDK(url);
-        PostmonCEPResponse response = sdk.getPostmon().getCepInformation("87045430");
+        PostmonCEPResponse response = sdk.getPostmon().getCepInformation("12948117");
         System.out.println(response.getCidade());
     }
 }
